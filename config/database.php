65abea4b45e1abdc9c52ddc2e5c8cf186258a4b0
Dilruba Köse <dilrubakose@gmail.com>
2019-12-31 01:09:39 +0200

    update database

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'remotemysql.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'nlWJRnyPGO'),
+            'username' => env('DB_USERNAME', 'nlWJRnyPGO'),
+            'password' => env('DB_PASSWORD', 'mrwEwkbqje'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
